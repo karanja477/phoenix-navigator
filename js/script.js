6cde120700514b3d657a5c24c9b6ece1eda2196c
karanja477git config --global user.email karanjakaruga89@gmail.com <karanjakaruga89@gmail.com>
2020-03-04 12:53:18 +0300

    styling of headers using css and initialized the JQuery function

--- js/script.js
+++ js/script.js
@@ -21,13 +21,46 @@ $(document).ready(function(){
   });
   
   $(document).ready(function(){
-    $("#maasai-market-image").click(function(){
-      $("#maasai-market-image").slideDown('1500').hide('1000');
+    $("#maasai-market").click(function(){
+      $("#maasai-market").slideDown('1500').hide('1000');
       $("#maasai").show('1500');
     });
     $("#maasai").click(function(){
       $("#maasai").slideUp('1500');
-      $("#maasai-market-image").slideDown('1500');
+      $("#maasai-market").slideDown('1500');
+    });
+  });
+
+  $(document).ready(function(){
+    $("#old-town").click(function(){
+      $("#old-town").slideDown('1500').hide('1000');
+      $("#old").show('1500');
+    });
+    $("#old").click(function(){
+      $("#old").slideUp('1500');
+      $("#old-town").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#ngara-market").click(function(){
+      $("#ngara-market").slideDown('1500').hide('1000');
+      $("#ngara").show('1500');
+    });
+    $("#ngara").click(function(){
+      $("#ngara").slideUp('1500');
+      $("#ngara-market").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#major-town").click(function(){
+      $("#major-town").slideDown('1500').hide('1000');
+      $("#major").show('1500');
+    });
+    $("#major").click(function(){
+      $("#major").slideUp('1500');
+      $("#major-town").slideDown('1500');
     });
   });
   
