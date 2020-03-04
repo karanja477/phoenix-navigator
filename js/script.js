e831352e345d5f4803d942cc1af1f12513de2481
Andrew59-boop <millerfaabi@gmail.com>
2020-03-04 10:39:16 +0300

    Feature(Description hide effect)

--- js/script.js
+++ js/script.js
@@ -19,5 +19,24 @@ $(document).ready(function(){
         $("#BUFFALO-T").slideUp('1500');
         $("#buffalo").slideDown('1500');
     });
+
+    $("#garden").click(function(){
+      $("#garden").slideDown('1500').hide('1000');
+      $("#GARDEN").show('1500');
+  });
+  $("#GARDEN").click(function(){
+      $("#garden").slideUp('1500');
+      $("#garden").slideDown('1500');
+  });
+
+  $("#junctin").click(function(){
+    $("#JUNCTION").slideDown('1500').hide('1000');
+    $("#JUNCTION").show('1500');
+});
+$("#JUNCTION").click(function(){
+    $("#JUNCTION").slideUp('1500');
+    $("#junction").slideDown('1500');
+});
+
 });
   
