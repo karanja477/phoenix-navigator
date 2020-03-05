0a88e9f7e1b74f85bb30e4301317ff01f9aa6452
karanja477git config --global user.email karanjakaruga89@gmail.com <karanjakaruga89@gmail.com>
2020-03-06 00:13:24 +0300

    changes success.html to markets.html

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "aleki" && password == "password"){
 alert ("Login successfully");
-window.location = "success.html"; // Redirecting to other page.
+window.location = "markets/markets.html"; // Redirecting to other page.
 return false;
 }
 else{
